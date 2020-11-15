444dec0f6b275b16c1aa40a74aad08519de356e8
Wolfara <58333086+Wolfara@users.noreply.github.com>
2020-11-15 16:02:44 +0700

    projectCreated

---
+++ AR-Quest/Assets/Scripts/InteractWithObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class InteractWithObject : MonoBehaviour
+{
+    [SerializeField] private Camera arCam;
+    [SerializeField] private ARRaycastManager _raycastManager;
+    List<ARRaycastHit> _hits = new List<ARRaycastHit>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = arCam.ScreenPointToRay(Input.mousePosition);
+            if(_raycastManager.Raycast(ray, _hits))
+            {
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if(hit.collider != null)
+                    {
+                        if(hit.collider.gameObject.tag=="Coin")
+                        {
+                            Destroy(hit.collider.gameObject);
+                        }
+                    } 
+                }
+            }
+        }
+    }
+}
---
+++ AR-Quest/Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> uliki = new List<GameObject>();
+    [SerializeField] private GameObject coins;
+    [SerializeField] private List<Transform> ulikiPositions = new List<Transform>();
+    [SerializeField] private List<Transform> coinsPositions = new List<Transform>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            Instantiate(uliki[i], ulikiPositions[i].position, ulikiPositions[i].rotation);
+        }
+        for(int i = 0; i < 5; i++)
+        {
+            Instantiate(coins, coinsPositions[i].position, coinsPositions[i].rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
